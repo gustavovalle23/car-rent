280d226b5e63a845146848dd19d03c0d3627c9a2
Gustavo Valle <gustavo.vallerp@hotmail.com>
2022-05-29 11:50:58 -0300

    adding default messages

---
+++ core/messages.py
@@ -0,0 +1,9 @@
+welcome_message: str = """
+Welcome to Chuck Norris Fun Facts!
+Obs: the default user is "admin" and the default password for this user is "admin"
+
+1. Login
+2. Register
+3. Sobre
+0. Exit
+"""
