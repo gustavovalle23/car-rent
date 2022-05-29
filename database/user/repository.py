26f509a200498c3e92993b1d56d412934bca4fc0
Gustavo Valle <gustavo.vallerp@hotmail.com>
2022-05-29 17:31:35 -0300

    type hint ajust

--- database/user/repository.py
+++ database/user/repository.py
@@ -1,7 +1,10 @@
 import sqlite3
+from typing import Optional, List
+
+from core.entity.user import User
 
 
-def create_user_table():
+def create_user_table() -> None:
     """
     create table if not exists user with
     name as varchar, username, password, email as varchar and phone as varchar
@@ -14,7 +17,7 @@ def create_user_table():
     conn.close()
 
 
-def create_user(name: str, username: str, password: str, email: str, phone: str):
+def create_user(name: str, username: str, password: str, email: str, phone: str) -> None:
     """ create new user with name, username, email and phone """
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
@@ -24,7 +27,7 @@ def create_user(name: str, username: str, password: str, email: str, phone: str)
     conn.close()
 
 
-def update_user(name, username, email, phone):
+def update_user(name: str, username: str, email: str, phone: str) -> None:
     """ update user with name, username, email and phone """
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
@@ -34,53 +37,47 @@ def update_user(name, username, email, phone):
     conn.close()
 
 
-def get_user_by_username(username):
+def get_user_by_username(username) -> Optional[User]:
     """ get user by username """
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
     c.execute("SELECT * FROM users WHERE username = ?", (username,))
-    user = c.fetchone()
+    user: Optional[User] = c.fetchone()
     return user
 
 
-def get_user_by_email(email):
+def get_user_by_email(email) -> Optional[User]:
     """ get user by email """
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
     c.execute("SELECT * FROM users WHERE email = ?", (email,))
-    user = c.fetchone()
+    user: Optional[User] = c.fetchone()
     return user
 
 
-def check_if_username_exists(username: str) -> bool:
+def check_if_username_exists(username: str) -> Optional[User]:
     """ check if username exists """
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
     c.execute("SELECT * FROM users WHERE username = ?", (username,))
     user = c.fetchone()
-    if user:
-        return True
-    else:
-        return False
+    return user
 
 
-def check_if_password_is_correct(username: str, password: str) -> bool:
+def check_if_password_is_correct(username: str, password: str) -> Optional[User]:
     """ check if password is correct """
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
     c.execute("SELECT * FROM users WHERE username = ? AND password = ?",
               (username, password))
-    user = c.fetchone()
-    if user:
-        return True
-    else:
-        return False
+    user: Optional[User] = c.fetchone()
+    return user
 
 
-def get_all_users():
+def get_all_users() -> Optional[List[User]]:
     """ get all users """
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
     c.execute("SELECT * FROM users")
-    users = c.fetchall()
+    users: Optional[List[User]] = c.fetchall()
     return users
