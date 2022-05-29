d1de34cbd52463bb670aa86c533e8b17c108ba4a
Gustavo Valle <gustavo.vallerp@hotmail.com>
2022-05-29 17:29:53 -0300

    adding create user table

--- core/checks/database_checks.py
+++ core/checks/database_checks.py
@@ -1,10 +1,13 @@
-from database.user import get_all_users
-from database.utils import initial_seed
+from database.user.repository import get_all_users
+from database.user.seed import initial_seed
 
 
 def create_tables():
-    from database.user import create_user_table
+    from database.user.repository import create_user_table
+    from database.car.repository import create_car_table
+
     create_user_table()
+    create_car_table()
 
 
 def initial_check():
